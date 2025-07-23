a6711437db34888820226fc1a8faaa4a3b8e4625
yoda0927 <yohann.remuszka@gmail.com>
2025-07-23 10:54:46 -0300

    Add files via upload

---
+++ exercicio01/script.js
@@ -0,0 +1,14 @@
+function soma(){
+    var numero1 = document.getElementById('n1').valueAsNumber
+    var numero2 = document.getElementById('n1').valueAsNumber
+    var resp = document.getElementById('resposta')
+    resp.textContent = numero1 + numero2
+    
+}
+
+function subtracao(){
+    var numero1 = document.getElementById('n4').valueAsNumber
+    var numero2 = document.getElementById('n3').valueAsNumber
+    var resp = document.getElementById('resposta')
+    resp.textContent = numero1 - numero2
+}
